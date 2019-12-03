7f39b36691871a18af77f6bf0e58cbcf078aaddf
Jake Angerman <jake@redislabs.com>
2019-12-03 08:55:34 -0500

    initial commit

---
+++ src/main/java/com/redislabs/redispringmusic/RedispringMusicApplication.java
@@ -0,0 +1,13 @@
+package com.redislabs.redispringmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedispringMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedispringMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redislabs/redispringmusic/RedispringMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redislabs.redispringmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedispringMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
