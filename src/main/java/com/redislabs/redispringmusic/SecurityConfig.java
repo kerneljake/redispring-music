87a35a53be250f6ec9c7ddc31cc38e489cecf91e
Jake Angerman <jake@redislabs.com>
2019-12-06 09:03:19 -0600

    csrf disabled just for the demo

--- src/main/java/com/redislabs/redispringmusic/SecurityConfig.java
+++ src/main/java/com/redislabs/redispringmusic/SecurityConfig.java
@@ -19,5 +19,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests().antMatchers("/").hasRole("ADMIN").anyRequest().authenticated();
+	http.csrf().disable(); // never do this in real life
     }
 }
